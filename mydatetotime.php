eb48d916fd9c6ff0834becc87ac7dba2b995eb1f
gbengazee <51008915+gbengazee@users.noreply.github.com>
2022-12-18 20:53:41 +0000

    Add files via upload

---
+++ mydatetotime.php
@@ -0,0 +1,8 @@
+<?php
+require_once("DB.php");
+//$db = new DB("localhost", "smyleusq_tb", "smyleusq_chat", "@china419");
+$db = new DB("127.0.0.1", "pwd_hash", "root", "");
+date_default_timezone_set('africa/lagos');
+
+ 
+?>
